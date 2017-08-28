4df53e7db68f8e2472dfa8eec009a38e140ac511
Corbit Ryan Sampson <sampsoc2@LabImage2016-17>
2017-08-28 16:31:34 -0700

    reduced the number of place functions called and addjusted layout. Also added binds for info bar

--- page_objects.py
+++ page_objects.py
@@ -17,6 +17,7 @@ class Start_Window:
         master.wm_title("Photovoltaic Characteristics")
         
         main_frame = Frame(master, width=250, height=250)
+        main_frame.pack()
 
         statement_label = Label(main_frame,text='Photovoltaic data processing\n and analysis')
         statement_label.place(anchor=CENTER,relx=0,rely=0,x=125,y=90)
@@ -29,17 +30,14 @@ class Start_Window:
         b.bind("<Enter>",self.infoupdate_process)
         b.bind("<Leave>",self.infoupdate_reset)
         b.grid(row=0)
-        
         Label(control_frame,width=10).grid(row=1)
-        
         b = Button(control_frame,text='Analyze Data', width=20)
         b.bind("<Enter>",self.infoupdate_analysis)
         b.bind("<Leave>",self.infoupdate_reset)
         b.grid(row=2)
-        
+
         control_frame.place(anchor=CENTER,relx=0,rely=0,x=125,y=150)
-        main_frame.pack()
-        
+
     def infoupdate_process(self,event):
         self.info.set("process")    
         
@@ -55,19 +53,25 @@ class Processing_Window:
 	def __init__(self,master):
 		
 		self.info = StringVar()
-		self.info.set("holder")		
+		self.info.set("holder")
 
-		top = Toplevel()
+		self.loc = StringVar()
+		self.loc.set("holder")
+
+		width =	master.winfo_screenwidth()
+		height = master.winfo_screenheight()	
+
+		top = Toplevel(width=1600,height=1000)
 		top.title("Processing Menu")
 
-		main_frame = Frame(top, width=1600,height=1000)
-		main_frame.pack()
+		main_frame = Frame(top)
+		main_frame.pack(fill=BOTH)
 
 		infobar = Label(top, textvariable=self.info,relief=SUNKEN,anchor=W)
 		infobar.pack(side=BOTTOM,fill=X)
 
-		process_frame = Frame(main_frame,width=100,height=400,bd=2, relief=SUNKEN)
-		process_frame.place(x=1200,y=600)
+		process_frame = Frame(main_frame,bd=2, relief=SUNKEN)
+		process_frame.grid(column=3,row=1,sticky=N)
 		l = Label(process_frame, text="Batchnumber")
 		l.grid(column=0,row=0, padx=2, pady=2,sticky=W)
 		e = Entry(process_frame)
@@ -77,15 +81,25 @@ class Processing_Window:
 		e = Entry(process_frame)
 		e.grid(column=1,row=1, padx=2, pady=2)
 		b = Button(process_frame,text="Run",width=10)
+		b.bind("<Enter>",self.infoupdate_runprocess)
+		b.bind("<Leave>",self.infoupdate_reset)
 		b.grid(column=1, row=2, padx=2, pady=2,sticky=W)
 
-		recur_frame = Frame(main_frame, width=490,height=570 ,bd=2,relief=SUNKEN)
-		recur_frame.place(x=1085,y=5)
+		recur_frame = Frame(main_frame,width=490,height=570,bd=2)
+		recur_frame.grid(column=3,row=0,pady=5,padx=5,sticky=N)
 
-		
-		# Fix how the controls are layed out. Not great
-		recur_control = Frame(recur_frame,bd=2,relief=SUNKEN,width=520)
-		recur_control.place(x=0,y=490)
+		recur_fig = Figure(figsize=(490/100,450/100),dpi=100)
+		recur_plot = recur_fig.add_subplot(1,1,1)
+
+		recur_canvas = FigureCanvasTkAgg(recur_fig, recur_frame)
+		recur_canvas.show()
+		recur_canvas.get_tk_widget().pack(side=TOP,fill=X)
+
+		recur_canvas._tkcanvas.pack(side=TOP,fill=X)	
+
+		recur_control = Frame(recur_frame,bd=2,relief=SUNKEN,width=490)
+		recur_control.pack(side=BOTTOM,fill=X)
+
 		l = Label(recur_control,text="Batchnumber")
 		l.grid(column=0,row=0,sticky=W,pady=1)
 		l = Label(recur_control,text="Devicenumber")
@@ -93,73 +107,107 @@ class Processing_Window:
 		l = Label(recur_control,text="Pixelnumber")
 		l.grid(column=2,row=0,sticky=W,pady=1)
 		
-		e = Entry(recur_control)
-		e.grid(column=0,row=1,pady=1)
-		e = Entry(recur_control)
-		e.grid(column=1,row=1,pady=1)
-		e = Entry(recur_control)
-		e.grid(column=2,row=1,pady=1)
 
-		b =Button(recur_control, text="Graph",width=15)
-		b.grid(column=0,row=3,pady=1)
+		entry_single = Frame(recur_control)
+		e = Entry(entry_single,width=15)
+		e.pack()
+		entry_single.grid(column=0,row=1,padx=1)
+		entry_single = Frame(recur_control)
+		e = Entry(entry_single,width=15)
+		e.pack()
+		entry_single.grid(column=1,row=1,padx=1)
+		entry_single = Frame(recur_control)
+		e = Entry(entry_single,width=15)
+		e.pack()
+		entry_single.grid(column=2,row=1,padx=1)
 
 
-		recur_fig = Figure(figsize=(490/100,490/100),dpi=100)
-		recur_plot = recur_fig.add_subplot(1,1,1)
+		b =Button(recur_control, text="Graph",width=5)
+		b.bind("<Enter>",self.infoupdate_recurgraph)
+		b.bind("<Leave>",self.infoupdate_reset)
+		b.grid(column=3,row=1,pady=1)
 
-		recur_canvas = FigureCanvasTkAgg(recur_fig, recur_frame)
-		recur_canvas.show()
-		recur_canvas.get_tk_widget().place(x=0,y=0)
-
-		recur_canvas._tkcanvas.place(x=0,y=0)
-		
 
 		graph_frame = Frame(main_frame,width=1055,height=1000)
-		graph_frame.place(x=5,y=0)		
-
-		graph1_frame = Frame(graph_frame,width=1055,height=490, bd=2,relief=SUNKEN)
-		graph1_frame.grid(row=0,padx=2,pady=5)
-
-		graph1_control = Frame(graph1_frame,bd=2,relief=SUNKEN,width=1055)
-		graph1_control.place(y=450)
-
-		l = Label(graph1_control,text="Batchnumber")
-		l.pack(side=LEFT,pady=2,padx=2)
-		e = Entry(graph1_control)
-		e.pack(side=LEFT,pady=2,padx=2)
-		b = Button(graph1_control, text="Graph")
-		b.pack(side=LEFT,pady=2,padx=2)
-		b = Button(graph1_control,text="Export")
-		b.pack(side=LEFT,pady=2,padx=2)
+		graph_frame.grid(column=0,row=0,columnspan=3,rowspan=2,padx=5)
+		
+		graph1_frame = Frame(graph_frame,width=1055,height=490, bd=2)
+		graph1_frame.bind("<Enter>",self.locupdate_graph1)
+		graph1_frame.grid(pady=5)
 
 		graph1_fig = Figure(figsize=(1055/100,450/100),dpi=100)
 		graph1_plot = graph1_fig.add_subplot(1,1,1)
 
 		graph1_canvas = FigureCanvasTkAgg(graph1_fig, graph1_frame)
 		graph1_canvas.show()
-		graph1_canvas._tkcanvas.place(x=0,y=0)
+		graph1_canvas._tkcanvas.pack(side=TOP,fill=X)
 
-		graph2_frame = Frame(graph_frame,width=1055,height=490, bd=2,relief=SUNKEN)
-		graph2_frame.grid(row=1,padx=2,pady=5)
+		graph1_control = Frame(graph1_frame,bd=2,relief=SUNKEN,width=1055)
+
+		graph1_control.pack(side=LEFT)
+
+		l = Label(graph1_control,text="Batchnumber")
+		l.pack(side=LEFT,pady=2,padx=2)
+		e = Entry(graph1_control,width=10)
+		e.pack(side=LEFT,pady=2,padx=2)
+		b = Button(graph1_control, text="Graph")
+		b.bind("<Enter>",self.infoupdate_graph)
+		b.bind("<Leave>",self.infoupdate_reset)
+		b.pack(side=LEFT,pady=2,padx=2)
+		b = Button(graph1_control,text="Export")
+		b.bind("<Enter>",self.infoupdate_export)
+		b.bind("<Leave>",self.infoupdate_reset)
+		b.pack(side=LEFT,pady=2,padx=2)
+
+		graph2_frame = Frame(graph_frame,width=1055,height=490, bd=2)
+		graph2_frame.bind("<Enter>",self.locupdate_graph2)
+		graph2_frame.grid(row=1,pady=5)
 
 		graph2_fig = Figure(figsize=(1055/100,450/100),dpi=100)
 		graph2_plot = graph2_fig.add_subplot(1,1,1)
 
 		graph2_canvas = FigureCanvasTkAgg(graph2_fig, graph2_frame)
 		graph2_canvas.show()
-		graph2_canvas._tkcanvas.place(x=0,y=0)
+		graph2_canvas._tkcanvas.pack(side=TOP,fill=X)
 
 		graph2_control = Frame(graph2_frame,bd=2,relief=SUNKEN,width=1055)
-		graph2_control.place(y=450)
+		graph2_control.pack(side=LEFT)
 
 		l = Label(graph2_control,text="Batchnumber")
 		l.pack(side=LEFT,pady=2,padx=2)
-		e = Entry(graph2_control)
+		e = Entry(graph2_control,width=10)
 		e.pack(side=LEFT,pady=2,padx=2)
 		b = Button(graph2_control, text="Graph")
+		b.bind("<Enter>",self.infoupdate_graph)
+		b.bind("<Leave>",self.infoupdate_reset)
 		b.pack(side=LEFT,pady=2,padx=2)
 		b = Button(graph2_control,text="Export")
+		b.bind("<Enter>",self.infoupdate_export)
+		b.bind("<Leave>",self.infoupdate_reset)
 		b.pack(side=LEFT,pady=2,padx=2)
+
+		
+	def infoupdate_recurgraph(self,event):
+	     self.info.set("Graphs the Recur graph of selected data")
+
+	def infoupdate_runprocess(self,event):
+	     self.info.set("Processes selected batch")
+
+	def locupdate_graph1(self,event):
+	     self.loc.set("Graph 1")
+	
+	def locupdate_graph2(self,event):
+	     self.loc_set("Graph 2")
+
+	def infoupdate_graph(self,event):
+	     self.info.set("graphs selected data on %s" % self.loc.get())
+
+	def infoupdate_export(self,event):
+	     self.info.set("Exports %s" % self.loc.get())		
+
+	def infoupdate_reset(self,event):
+	     self.info.set("holder")
+
 
 		
 
--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@ from tkinter import *
 import page_objects as po
 root = Tk()
 
+window1 = po.Start_Window(root)
 window2 = po.Processing_Window(root)
 
 root.mainloop()
