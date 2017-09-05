e8da2349b2551a3a20e88878f1a5b6926cc2b848
Corbit Ryan Sampson <sampsoc2@LabImage2016-17>
2017-09-05 14:16:11 -0700

    Adjusted weights for column layout. Should help with other screens. Needs to be tested. next step either set up or improve layout.

--- page_objects.py
+++ page_objects.py
@@ -1,7 +1,10 @@
 # These are the files that need to be imported
 
-from tkinter import * # Python 3
-# from Tkinter import * # Python 2
+try:
+	from tkinter import * # Python 3
+except ImportError:
+	from Tkinter import * # Python 2
+
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -61,17 +64,23 @@ class Processing_Window:
 		width =	master.winfo_screenwidth()
 		height = master.winfo_screenheight()	
 
-		top = Toplevel(width=1600,height=1000)
+		top = Toplevel()
 		top.title("Processing Menu")
+		top.geometry("%ix%i" % (width,height))
 
 		main_frame = Frame(top)
-		main_frame.pack(fill=BOTH)
+		main_frame.pack(fill=BOTH,expand=True)
+		main_frame.columnconfigure(0, weight=75)
+		main_frame.columnconfigure(1,weight=25)
+		main_frame.rowconfigure(0,weight=50)
+		main_frame.rowconfigure(1,weight=50)
 
 		infobar = Label(top, textvariable=self.info,relief=SUNKEN,anchor=W)
 		infobar.pack(side=BOTTOM,fill=X)
 
 		process_frame = Frame(main_frame,bd=2, relief=SUNKEN)
-		process_frame.grid(column=3,row=1,sticky=N)
+		process_frame.grid(column=1,row=1,pady=5,padx=5,sticky=N+S+E+W)
+		
 		l = Label(process_frame, text="Batchnumber")
 		l.grid(column=0,row=0, padx=2, pady=2,sticky=W)
 		e = Entry(process_frame)
@@ -85,8 +94,8 @@ class Processing_Window:
 		b.bind("<Leave>",self.infoupdate_reset)
 		b.grid(column=1, row=2, padx=2, pady=2,sticky=W)
 
-		recur_frame = Frame(main_frame,width=490,height=570,bd=2)
-		recur_frame.grid(column=3,row=0,pady=5,padx=5,sticky=N)
+		recur_frame = Frame(main_frame,bd=2)
+		recur_frame.grid(column=1,row=0,pady=5,padx=5,sticky=N+S+E+W)
 
 		recur_fig = Figure(figsize=(490/100,450/100),dpi=100)
 		recur_plot = recur_fig.add_subplot(1,1,1)
@@ -94,11 +103,15 @@ class Processing_Window:
 		recur_canvas = FigureCanvasTkAgg(recur_fig, recur_frame)
 		recur_canvas.show()
 		recur_canvas.get_tk_widget().pack(side=TOP,fill=X)
-
 		recur_canvas._tkcanvas.pack(side=TOP,fill=X)	
 
-		recur_control = Frame(recur_frame,bd=2,relief=SUNKEN,width=490)
-		recur_control.pack(side=BOTTOM,fill=X)
+		
+		recur_control = Frame(recur_frame,bd=2,relief=SUNKEN)
+		recur_control.pack(side=TOP,fill=X)
+		recur_control.columnconfigure(0,weight=25)
+		recur_control.columnconfigure(1,weight=25)
+		recur_control.columnconfigure(2,weight=25)
+		recur_control.columnconfigure(3,weight=25)
 
 		l = Label(recur_control,text="Batchnumber")
 		l.grid(column=0,row=0,sticky=W,pady=1)
@@ -109,41 +122,38 @@ class Processing_Window:
 		
 
 		entry_single = Frame(recur_control)
-		e = Entry(entry_single,width=15)
+		e = Entry(entry_single,width=10)
 		e.pack()
-		entry_single.grid(column=0,row=1,padx=1)
+		entry_single.grid(column=0,row=1,padx=1,stick=W)
 		entry_single = Frame(recur_control)
-		e = Entry(entry_single,width=15)
+		e = Entry(entry_single,width=10)
 		e.pack()
-		entry_single.grid(column=1,row=1,padx=1)
+		entry_single.grid(column=1,row=1,padx=1,stick=W)
 		entry_single = Frame(recur_control)
-		e = Entry(entry_single,width=15)
+		e = Entry(entry_single,width=10)
 		e.pack()
-		entry_single.grid(column=2,row=1,padx=1)
+		entry_single.grid(column=2,row=1,padx=1,stick=W)
 
 
 		b =Button(recur_control, text="Graph",width=5)
 		b.bind("<Enter>",self.infoupdate_recurgraph)
 		b.bind("<Leave>",self.infoupdate_reset)
-		b.grid(column=3,row=1,pady=1)
-
-
-		graph_frame = Frame(main_frame,width=1055,height=1000)
-		graph_frame.grid(column=0,row=0,columnspan=3,rowspan=2,padx=5)
+		b.grid(column=3,row=0,rowspan=2,pady=1,padx=1)
 		
-		graph1_frame = Frame(graph_frame,width=1055,height=490, bd=2)
+
+		graph1_frame = Frame(main_frame,bd=2)
 		graph1_frame.bind("<Enter>",self.locupdate_graph1)
-		graph1_frame.grid(pady=5)
+		graph1_frame.grid(column=0, row=0, pady=5,sticky=N+S+W+E)
 
 		graph1_fig = Figure(figsize=(1055/100,450/100),dpi=100)
 		graph1_plot = graph1_fig.add_subplot(1,1,1)
 
 		graph1_canvas = FigureCanvasTkAgg(graph1_fig, graph1_frame)
 		graph1_canvas.show()
-		graph1_canvas._tkcanvas.pack(side=TOP,fill=X)
+		graph1_canvas.get_tk_widget().pack(side=TOP,fill=BOTH)
+		graph1_canvas._tkcanvas.pack(side=TOP,fill=BOTH)
 
-		graph1_control = Frame(graph1_frame,bd=2,relief=SUNKEN,width=1055)
-
+		graph1_control = Frame(graph1_frame,bd=2,relief=SUNKEN)
 		graph1_control.pack(side=LEFT)
 
 		l = Label(graph1_control,text="Batchnumber")
@@ -159,18 +169,19 @@ class Processing_Window:
 		b.bind("<Leave>",self.infoupdate_reset)
 		b.pack(side=LEFT,pady=2,padx=2)
 
-		graph2_frame = Frame(graph_frame,width=1055,height=490, bd=2)
+		graph2_frame = Frame(main_frame,bd=2)		
 		graph2_frame.bind("<Enter>",self.locupdate_graph2)
-		graph2_frame.grid(row=1,pady=5)
+		graph2_frame.grid(column=0,row=1,pady=5,sticky=N+S+E+W)
 
 		graph2_fig = Figure(figsize=(1055/100,450/100),dpi=100)
 		graph2_plot = graph2_fig.add_subplot(1,1,1)
 
 		graph2_canvas = FigureCanvasTkAgg(graph2_fig, graph2_frame)
 		graph2_canvas.show()
+		graph2_canvas.get_tk_widget().pack(side=TOP,fill=BOTH)
 		graph2_canvas._tkcanvas.pack(side=TOP,fill=X)
 
-		graph2_control = Frame(graph2_frame,bd=2,relief=SUNKEN,width=1055)
+		graph2_control = Frame(graph2_frame,bd=2,relief=SUNKEN)
 		graph2_control.pack(side=LEFT)
 
 		l = Label(graph2_control,text="Batchnumber")
